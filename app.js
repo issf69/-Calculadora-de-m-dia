23053b724800a8611ccf7e0613d40e4f0bdbe26b
Yrene Sylva França <105497075+issf69@users.noreply.github.com>
2022-09-19 05:27:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var notaDoPrimeiroBimestre = 9;
+var notaDoSegundoBimestre = 7;
+var notaDoTerceirooBimestre = 4;
+var notaDoQuartoBimestre = 2;
+
+var notaFinal =
+    (notaDoPrimeiroBimestre +
+        notaDoSegundoBimestre +
+        notaDoTerceirooBimestre +
+        notaDoQuartoBimestre) /
+    4;
+
+var notaFixada = notaFinal.toFixed(1);
+
+console.log(" Bem Vindo " + nome);
+console.log(notaFixada);
+
+//isso é um comentario
+
+//Revisão
+// Variáveis,Strings,console.log,toFixed,operações,
+//matemática,concatenação
